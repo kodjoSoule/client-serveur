cc870981f29846029da10b6b3165fe0a5ca24c16
= <souleymanekodjo@gmail.com>
2022-12-26 03:29:36 +0000

    add new function voireStatistique function

--- server.py
+++ server.py
@@ -10,8 +10,14 @@ FORMAT = "utf8"
 lock = threading.Lock()
 
 # Variable partagée
-statistiques = {'fact':0,'fib':0,'add':0, 'sous':0,'div':0,'mult':0}
-
+#statistiques = {'fact':0,'fib':0,'add':0, 'sous':0,'div':0,'mult':0}
+#ou en plusieur variable
+nbreFact = 0
+nbreFib = 0
+nbreAdd = 0
+nbreSous = 0
+nbreMult = 0
+nbreDiv = 0
 #Creer une socket TCP
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #Liez la socket a une adresse et un port
@@ -23,29 +29,62 @@ server_socket.listen(5)
 print("En ecoute")
 
 #Declaration des fonction
-
+def voireStatistique():
+    global nbreFact
+    global nbreFib
+    global nbreAdd
+    global nbreSous
+    global nbreMult
+    global nbreDiv
+    print("********************************")
+    print("\les statistiques des fonction\n")
+    print("Addtion : {}".format(nbreAdd))
+    print("Soustraton : {}".format(nbreSous))
+    print("Division : {}".format(nbreDiv))
+    print("Multiplication : {}".format(nbreMult))
+    print("Fibbonaci : {}".format(nbreFib))
+    print("Factoriel: {}".format(nbreFact))
 def faireOperation(message_server, client_socket):
     operation, *args = message_server.split()
-    print("\n Operation : {}".format(operation))
     #conversion des arguments en entiers
     args = [int(arg) for arg in args]
+    
     if operation == '1':
+        lock.acquire()
         resultat = addition(*args)
+        global nbreAdd
+        nbreAdd += 1
     elif operation == '2':
+        lock.acquire()
         resultat = soust(*args)
+        global nbreSous
+        nbreSous += 1
     elif operation == '3':
+        lock.acquire()
         resultat = div(*args)
+        global nbreDiv
+        nbreDiv += 1
     elif operation == '4':
+        lock.acquire()
         resultat = mult(*args)
+        global nbreMult
+        nbreMult += 1
     elif operation == '5':
+        lock.acquire()
         resultat = fact(*args)
+        global nbreFact
+        nbreFact += 1
     elif operation == '6':
+        lock.acquire()
         resultat = fibonacci(*args)
+        global nbreFib
+        nbreFib += 1
     else:
         print("Rien a faire")
     # Envoi du résultat au client
     client_socket.send(str(resultat).encode())
-
+    # Libération du verrou
+    lock.release()
 def gerer_client(client,adrr) :
     print(f'> {adrr} client : connecté')
     while True :
@@ -55,28 +94,9 @@ def gerer_client(client,adrr) :
             print(f"Le client {adrr}est deconnecté ")
             break
         else:
-            #print("[\n Message recu de  {} client : {}".format(adrr, message_server))
-            #print("\n]")
-            operation, *args = message_server.split()
-            print("\n Operation : {}".format(operation))
-            #conversion des arguments en entiers
-            args = [int(arg) for arg in args]
-            if operation == '1':
-                resultat = addition(*args)
-            elif operation == '2':
-                resultat = soust(*args)
-            elif operation == '3':
-                resultat = div(*args)
-            elif operation == '4':
-                resultat = mult(*args)
-            elif operation == '5':
-                resultat = fact(*args)
-            elif operation == '6':
-                resultat = fibonacci(*args)
-            else:
-                print("Rien a faire")
-            # Envoi du résultat au client
-            client_socket.send(str(resultat).encode())
+            faireOperation(message_server, client)
+            voireStatistique()
+            
             
 
 #FIN Declaration des fonction
